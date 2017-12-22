49a9f7be280621d606cbf0b5764a49296e0aadbf
Kaioru <keifuurawrs@gmail.com>
2017-12-22 12:53:29 +0800

    Rewrite project to generate functions and configs for memefy

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const run = require('gulp-run');
+
+require('load-gulp-tasks')(gulp, {
+  pattern: ['tasks/**/*.js']
+});
+
+gulp.task('start', ['default'], function() {
+  return run('sls offline start', {
+    cwd: './dist',
+    verbosity: 3,
+    silent: false
+  }).exec();
+});
+
+gulp.task('default', ['generate:config', 'generate:handler']);
